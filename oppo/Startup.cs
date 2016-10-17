384cf9e367e05630c84220efd7191520d26c9eb6
stu <stu@qq.com>
2016-10-17 16:08:22 +0800

    添加项目文件。

---
+++ oppo/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(oppo.Startup))]
+namespace oppo
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
